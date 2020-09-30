5a1177c7a12ab4b0d605af2bf65b94afbcb87124
sunarko.EAM <sunarko.EAM@indonesiapower.co.id>
2020-09-30 09:30:54 +0700

    edit homepage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Selamat Datang Di aplikasi react ini
+          Welcome to React App
         </p>
       </header>
     </div>

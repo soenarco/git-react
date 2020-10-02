8c3c851f9230d401d2e23d791ac2850910f929eb
soenarco <sunarkocindaga@gmail.com>
2020-10-02 09:34:35 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to React App
+          Halo , sunarko
         </p>
       </header>
     </div>
